1fec827d5a28d436e1e7ffb2351cda1fd46a2f99
Iann Isacksson <iisackssonoliveira@gmail.com>
2019-10-28 20:18:17 -0300

    Cadastrando Notificacoes no DB

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import User from '../models/User';
 import File from '../models/File';
 import Appointment from '../models/Appointment';
+import Notification from '../schema/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -71,6 +73,10 @@ class AppointmentController {
       },
     });
 
+    /**
+     * Check date availability
+     */
+
     if (checkAvailability) {
       return res
         .status(400)
@@ -81,6 +87,22 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', as' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo Agendamento de ${user.name} para ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
